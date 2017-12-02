ac46f10eee53bc3f5610590f25ad89ac53004e22
warsawpact <kaiwenli@college.harvard.edu>
2017-12-02 16:51:07 -0500

    Warnings for parallel fifths and eighths

--- theorybot.py
+++ theorybot.py
@@ -11,22 +11,28 @@ except IndexError:
 
 tree = ET.parse(input)
 
+# Info that is parsed from XML file
 notes = []
 intervals = []
-
-highest_voice = 0
-for voice in tree.iter(tag='voice'):
-    if voice.text > highest_voice:
-        highest_voice = voice.text
-    
-print("Highest voice: " + highest_voice)
 
 for pitch in tree.findall(".//pitch"):
     notes.append([pitch.find('step').text, pitch.find('octave').text])
 
-print(notes)
-
 for i in range(len(notes) / 2):
-    intervals.append((ord(notes[i][0]) + 2 % 7) - ((ord(notes[len(notes) / 2 + i][0])) + 2 % 7))
+    interval = (ord(notes[i][0]) + 2 % 7) - ((ord(notes[len(notes) / 2 + i][0])) + 2 % 7) + 1
+    if interval < 0:
+        interval += 7;
+    intervals.append(interval)
 
-print(intervals)
+
+print("Intervals:")
+for i in range(len(intervals) - 1):
+    print(intervals[i])
+    if intervals[i] == intervals[i + 1]:
+        if intervals[i] == 5:
+            print("\tWarning: repeated fifths. Check to make sure they are not in parallel motion.")
+        elif intervals[i] == 8:
+            print("Warning: repeated eighths. Check to make sure they are not in parallel motion.")
+
+
+
