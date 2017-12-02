804ddd9e650935a2f305deeb52b72401728edece
warsawpact <kaiwenli@college.harvard.edu>
2017-12-02 09:51:36 -0500

    Basic parsing added

--- theorybot.py
+++ theorybot.py
@@ -9,6 +9,17 @@ except IndexError:
     print("Usage: ./theorybot.py <file>")
     sys.exit(1)
 
-with open(input, 'rU') as i:
-    for line in i:
-        print(line)
+tree = ET.parse(input)
+
+#for elem in tree.iter(tag='step'):
+#    print(elem.text)
+
+for pitch in tree.findall(".//pitch"):
+    print(pitch.find('step').text)
+    print(pitch.find('octave').text)
+    
+# Use if-statements to determine which array the notes will go under    
+# print(note.find('voice').text) 
+
+# Have two counters and increment when you add up duration a certain way
+# print(note.find('duration').text)
