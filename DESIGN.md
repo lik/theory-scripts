d6ea0dddd36de1611b89b5370c1b15743edafd4d
warsawpact <kaiwenli@college.harvard.edu>
2017-12-07 04:32:27 -0500

    info.py and test files

---
+++ DESIGN.md
@@ -0,0 +1,3 @@
+# errorcheck.py
+
+I used the ElementTree API to parse through the XML file. Took a while to get ahold of but using an API did make things more convenient. I then decided to use two lists to store the pertinent info from the XML file that is to be used later so I don't have to interact with swaths of information that are not necessary for the this script's purposes. 
---
+++ info.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+import xml.etree.ElementTree as ET
+
+try:
+    input = sys.argv[1]
+except IndexError:
+    print("Usage: ./info.py <file>")
+    sys.exit(1)
+
+tree = ET.parse(input)
+
+measures = 0;
+for measure in tree.findall(".//measure"):
+    measures += 1
+
+"""
+Converts the units used in the tempo listing into a number
+For simplicity only supporting the most commonly used ones
+"""
+beat_unit = tree.find(".//beat-unit").text
+if beat_unit == "whole":
+    beat_unit = 1
+elif beat_unit == "half":
+    beat_unit = 2
+elif beat_unit == "quarter":
+    beat_unit = 4
+elif beat_unit == "eighth":
+    beat_unit = 8
+else:
+    print("Beat-unit not supported")
+    sys.exit(1)
+
+bpm = float(tree.find(".//per-minute").text)
+if bpm == "":
+    print("Beats per measure not found")
+    sys.exit(1)
+
+numerator = int(tree.find(".//beats").text)
+denominator = int(tree.find(".//beat-type").text)
+
+time = denominator / beat_unit * (1 / bpm) * numerator * measures
+
+print("This piece if played to the marked tempo will take %f minutes" % (time))
